aed3bdbe6b73f978d8a69a67def81b424e8a43d3
jamesjosephreynolds <jamesjosephreynolds@gmail.com>
2017-01-30 15:27:05 -0500

    Non working model
    Car goes off road at dirt patch, swerves to avoid the shadow of a tree.

---
+++ custom_functions.py
@@ -0,0 +1,204 @@
+''' This is a utility file to define functions
+    model.py will reference these functions to
+    have a clean layout and improved readability '''
+
+import numpy as np
+import csv
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+def check_custom_functions_import():
+    print("custom_functions.py loaded successfully")
+
+def get_list_from_csv(file):
+    # get a list of elements from a csv file
+
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        return_list = list(reader)
+
+    return return_list
+
+def get_col_data_from_list(list_data, col_idx, row_idx):
+    # get column data from list as array of real numbers
+
+    col_np_array = np.zeros((1), dtype = np.float32)
+
+    col_np_array = float(list_data[row_idx][col_idx])
+
+    return col_np_array
+
+
+def get_img_file_from_list(list_data, col_idx, row_idx):
+    # get image data from list as array of images
+
+    img_list = list_data[row_idx][col_idx]
+
+    return img_list
+
+def get_img_from_file(file):
+    # take filename and path and load into a numpy array
+    img = cv2.imread(file, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
+    
+
+def get_prob(array, threshold):
+    # calculate the probability of points in an array smaller than a threshold
+
+    N = len(array)
+    sum = 0
+    print(N)
+
+    for point in array:
+        if abs(point) < threshold:
+            sum+=1
+
+    print("probability of zero is {:1.3f}".format(sum/N))
+    return sum / N
+
+def norm_data(data):
+    norm_data = np.zeros_like(data, dtype = np.float32)
+    norm_data = data/255 - 0.5
+
+    return norm_data
+
+def visualize(img):
+    plt.imshow(img)
+    plt.show()
+
+    return 1
+
+def crop_img(img, shiftV, shiftH, hood, sky, size = (66,200)):
+    # crop the image around the remaining non-black area
+    # after vertical and horizontal translation
+    rows, cols, _ = img.shape
+
+    if shiftV < -sky:
+        y0 = 0
+    else:
+        y0 = sky + shiftV
+
+    if shiftV > hood:
+        y1 = rows
+    else:
+        y1 = rows - (hood - shiftV)
+
+    if shiftH < 0:
+        x0 = 0
+    else:
+        x0 = shiftH
+
+    if shiftH > 0:
+        x1 = cols
+    else:
+        x1 = cols + shiftH
+
+    # find the center pixel of the smaller image
+    center = (np.int((y1+y0)/2), np.int((x1+x0)/2))
+
+    # find the corners of the smaller image
+    y0 = np.int(center[0] - size[0]/2)
+    y1 = np.int(center[0] + size[0]/2)
+    x0 = np.int(center[1] - size[1]/2)
+    x1 = np.int(center[1] + size[1]/2)
+
+    # grab the smaller image
+    img = img[y0:y1, x0:x1]
+
+    # redundant resize in case rounding generates +/- 1 pixel error
+    img = resize(img, size[1], size[0])
+    
+    return img
+
+def vertical_shift(img,maxY = 30):
+    # return the image, randomly shifted up to max pixels
+    # return shift as feedback indicating the magnitude
+    rows, cols, _ = img.shape
+    shift = np.int(maxY/15)*np.int(np.random.uniform(-15, 15, 1))
+    M = np.float32([[1, 0, 0],[0, 1, shift]])
+    img = cv2.warpAffine(img, M, (cols, rows))
+
+    return img, shift
+
+def horizontal_shift(img,maxX = 40):
+    # return the image, randomly shifted up to max pixels
+    # return shift as feedback indicating the magnitude
+    rows, cols, _ = img.shape
+    shift = np.int(maxX/15)*np.int(np.random.uniform(-15, 15, 1))
+    M = np.float32([[1, 0, shift],[0, 1, 0]])
+    img = cv2.warpAffine(img, M, (cols, rows))
+
+    return img, shift
+
+def brightness_shift(img,maxB = 1.25):
+    # return the image, randomly shifted scaled in brightness
+    maxB = max(maxB, 1)
+    shift = np.random.uniform(2-maxB, maxB, 1)
+    img[:,:,2] = img[:,:,2]*shift
+
+    return img, shift
+
+def resize(img, width = 120, height = 120):
+    img = cv2.resize(img,(width, height), interpolation = cv2.INTER_CUBIC)
+    return img
+
+def get_data_arrays(array):
+    # load images and steering angles into data arrays
+    N = len(array)
+    y_array = np.zeros((1, 1), dtype = np.float32)
+    X = ()
+
+
+    left_bias = 0.2
+    right_bias = -0.2
+    inner_idx = 0
+    
+    for outer_idx in range(N):
+        #for inner_idx in range(3):
+        x_file = get_img_file_from_list(array, inner_idx, outer_idx)
+            #if inner_idx is 0:
+        y = get_col_data_from_list(array, 3, outer_idx)
+            #elif inner_idx is 1:
+                #y = get_col_data_from_list(array, 3, outer_idx) + left_bias
+            #elif inner_idx is 2:
+                #y = get_col_data_from_list(array, 3, outer_idx) + right_bias
+
+        y = np.array([y], dtype = np.float32)
+            
+        if outer_idx is 0:# and inner_idx is 0:
+            X_array = [x_file]
+            y_array[0] = y
+        else:
+            X_array.append(x_file)
+            y_array = np.append(y_array, y)
+                
+    return X_array, y_array
+
+def pre_process(img, steer, gain, maxY = 30, maxX = 40, maxB = 1.25, hood = 15, sky = 50, size = (66, 200), mode = 'train'):
+    if mode is 'train':
+        img, shiftV = vertical_shift(img, maxY)
+        img, shiftH = horizontal_shift(img, maxX)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        img, shiftB = brightness_shift(img, maxB)
+        img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+        if maxX is not 0:
+            steer += (shiftH * gain / maxX) # adjust steering angle for horizontal shift
+        else:
+            steer = steer
+    else:
+        shiftV = 0
+        shiftH = 0
+        shiftB = 1
+        steer = steer
+
+    img = crop_img(img,  shiftV, shiftH, hood, sky)
+    flip_prob = np.random.uniform(-1, 1, 1)
+    if flip_prob > 0 and mode is 'train':
+        img = cv2.flip(img, 1)
+        steer = -steer
+    
+    return img, steer, shiftV, shiftH, shiftB
---
+++ model.py
@@ -0,0 +1,144 @@
+'''
+This script takes CSV data from the driving simulator
+and uses the Keras module to train a CNN that outputs
+desired steering angle
+'''
+
+'''Import modules'''
+import numpy as np
+import tensorflow as tf
+import cv2
+import custom_functions as cf
+from keras.layers.core import Flatten, Dense, Activation, Dropout
+from keras.activations import relu, softmax
+from keras.models import Model, Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+import json
+from sklearn.model_selection import train_test_split
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+cf.check_custom_functions_import()
+
+'''Load data using utility module custom_functions.py'''
+drive_data = cf.get_list_from_csv('driving_log.csv')
+X_data_file, y_data = cf.get_data_arrays(drive_data)
+X_train_file, X_validation_file, y_train, y_validation = train_test_split(X_data_file, y_data,
+                                        test_size = 0.2, random_state=0)
+
+
+if 0: #debug switch visualize pre-processing
+
+    X_data_in = cf.get_img_from_file(X_train_file[0])
+    y_data_in = y_train[0]
+    fig = plt.figure()
+    ax1 = fig.add_subplot(4,4,1)
+    ax1.imshow(X_data_in)
+    ax1.set_xlabel('steer = '+str(y_data_in))
+    
+    for loop in range(2,17):
+        X_data, y_data, shiftV, shiftH, shiftB, = cf.pre_process(X_data_in, y_data_in, 0.5)
+        axN = fig.add_subplot(4,4,loop)
+        axN.imshow(X_data)
+        axN.set_xlabel('steer = '+str(y_data))
+        
+    plt.show()
+
+'''Define data generator'''
+def data_generator(X_file, y, batch_size = 256, width = 200, height = 66):
+
+    X_batch = np.zeros((batch_size, height, width, 3), dtype = np.float32)
+    y_batch = np.zeros(batch_size, dtype = np.float32)
+
+    N = len(y)
+    data_idx = 0
+    while 1:
+        for batch_idx in range(batch_size):
+            if data_idx >= N:
+                data_idx = 0
+
+            keep_prob = 0
+            while keep_prob < 0.9:
+                steer = y[data_idx]
+                img = cf.get_img_from_file(X_file[data_idx])
+                if steer < 0.1:
+                    keep_prob = np.random.uniform(0,1,1)
+                    data_idx += 1
+                    if data_idx >= N:
+                        data_idx = 0
+                else:
+                    keep_prob = 1
+                
+            if steer < 0.1:
+                maxX = 30
+            elif steer < 0.2:
+                maxX = 20
+            elif steer < 0.3:
+                maxX = 10
+            else:                      
+                maxX = 0
+
+            img, steer, _, _, _ = cf.pre_process(img, steer, 0.7, maxX = maxX)
+
+            X_batch[batch_idx] = cf.norm_data(img)
+            y_batch[batch_idx] = steer
+            data_idx += 1
+
+        yield X_batch, y_batch
+
+'''Define CNN model architecture with Keras
+    use example from Nvidia End to End Learning for Self-Driving Cars
+    as reference for the initial model architecture
+    use max pooling instead of 2x2 stride, as suggested by Vivek
+    use 3x3 kernel instead of 5x5 for computational simplicity
+    start with 3 convolution layers, 3 fully connected'''
+drive_model = Sequential()
+
+drive_model.add(Convolution2D(24, 5, 5, input_shape = (66, 200, 3), activation='relu'))
+drive_model.add(MaxPooling2D((2, 2), border_mode = 'same'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Convolution2D(36, 5, 5, activation='relu'))
+drive_model.add(MaxPooling2D((2, 2), border_mode = 'same'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Convolution2D(48, 5, 5, activation='relu'))
+drive_model.add(MaxPooling2D((2, 2), border_mode = 'same'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Convolution2D(64, 3, 3, activation='relu'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Convolution2D(64, 3, 3, activation='relu'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Flatten())
+
+drive_model.add(Dense(100,  activation='relu'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Dense(50,  activation='relu'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Dense(10, activation='tanh'))
+drive_model.add(Dropout(0.5))
+
+drive_model.add(Dense(1))
+
+if 0: # debug switch show model summary
+    drive_model.summary()
+
+if 1: # debug switch train model
+    '''Train the model'''
+    drive_model.compile(optimizer='Adam', loss='mse', lr = 0.0001)
+    drive_model.load_weights('model.h5', by_name=False)
+    drive_model.fit_generator(data_generator(X_train_file, y_train),
+                              validation_data = data_generator(X_validation_file, y_validation),
+                              nb_val_samples = 2048, samples_per_epoch = 16384,
+                              nb_epoch = 5, verbose = 1)
+
+drive_model.save_weights('model.h5')
+model_json = drive_model.to_json()
+json.dump(model_json, open('model.json', 'w+'))
